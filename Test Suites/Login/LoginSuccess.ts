dad13644854b3170f910fbe2ac3400c254878ec7
Hiếu Nguyễn Văn <hieunv@icitech.net>
2023-10-22 11:31:27 +0700

    Login + Sqli

---
+++ Test Suites/Login/LoginSuccess.ts
@@ -0,0 +1,14 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<TestSuiteEntity>
+   <description></description>
+   <name>LoginSuccess</name>
+   <tag></tag>
+   <isRerun>false</isRerun>
+   <mailRecipient></mailRecipient>
+   <numberOfRerun>0</numberOfRerun>
+   <pageLoadTimeout>30</pageLoadTimeout>
+   <pageLoadTimeoutDefault>true</pageLoadTimeoutDefault>
+   <rerunFailedTestCasesOnly>false</rerunFailedTestCasesOnly>
+   <rerunImmediately>false</rerunImmediately>
+   <testSuiteGuid>10382f9c-0373-4347-9bd7-89347911ec09</testSuiteGuid>
+</TestSuiteEntity>
